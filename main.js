543173c184a16d56e1e2b086c981ce41bbef0cf5
allansousadev <allansousa.dev@gmail.com>
2024-01-10 15:23:59 -0300

    Substituindo dados do html para javascript

--- main.js
+++ main.js
@@ -1,8 +1,100 @@
 const Modal = {
-    open(){
-        document.querySelector('.modal-overlay').classList.add('active')
-    },
-    close(){
-        document.querySelector('.modal-overlay').classList.remove('active')
-    }
-}
+	toggle() {
+		const modalOverlay = document.querySelector('.modal-overlay');
+		modalOverlay.classList.toggle('active');
+	},
+
+	open() {
+		document.querySelector('.modal-overlay').classList.add('active');
+	},
+
+	close() {
+		document.querySelector('.modal-overlay').classList.remove('active');
+	},
+};
+
+const transactions = [
+	{
+		id: 1,
+		description: 'Luz',
+		amount: -40000,
+		date: '10/01/2023',
+	},
+	{
+		id: 2,
+		description: 'Website',
+		amount: 500000,
+		date: '10/01/2023',
+	},
+	{
+		id: 3,
+		description: 'Internet',
+		amount: -25000,
+		date: '10/01/2023',
+	},
+	{
+		id: 4,
+		description: 'App',
+		amount: 600000,
+		date: '10/01/2023',
+	},
+];
+
+const Transaction = {
+	incomes() {
+		// Somar as entradas
+	},
+	expenses() {
+		// Somar as saídas
+	},
+	total() {
+		// Entradas - Saídas
+	},
+};
+
+const Utils = {
+	formatCurrency(value) {
+		const signal = Number(value) < 0 ? "-" : ""
+		
+		value = String(value).replace(/\D/g, "")
+
+		value = Number(value) / 100
+
+		value = value.toLocaleString("pt-BR", {
+			style: "currency",
+			currency: "BRL"
+		})
+
+		return signal + value
+
+	}
+}  
+const DOM = {
+	transactionsContainer: document.querySelector('#data-table tbody'),
+	addTransaction(transaction, index) {
+		const tr = document.createElement('tr')
+		tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+		DOM.transactionsContainer.appendChild(tr)
+	},
+
+	innerHTMLTransaction(transaction) {
+		const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+		const amount = Utils.formatCurrency(transaction.amount)
+
+		const html = `
+        	<td class="description">${transaction.description}</td>
+        	<td class="${CSSclass}">${amount}</td>
+        	<td class="date">${transaction.date}</td>
+        	<td>
+            	<img src="./assets/minus.svg" alt="Remover transação">
+        	</td>
+        `
+		return html
+	},
+};
+
+transactions.forEach(function(transaction) {
+	DOM.addTransaction(transaction)
+})
